0bc8cb9a26d7bacb612b026fba0f5012f503d418
Tomas <tomas.sl@hotmail.com>
2018-10-16 23:42:56 -0300

    Agrego informacion de contacto.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,187 +2,113 @@ import React from 'react'
 import Link from 'gatsby-link'
 import styled, { keyframes } from 'styled-components'
 import { palette } from '../utils/theme'
-import Yoni from '../static/images/yoni.jpg'
-
-const borderGrow = keyframes`
- from {
-    transform: rotateX(0deg) scale(0.9);
- }
- to {
-   opacity: 1;
-   transform: rotateX(70deg) scale(1);
- }
-`
+import Email from '../static/icons/email.svg'
+import Phone from '../static/icons/phone.svg'
+import Github from '../static/icons/github.svg'
+import LinkedIn from '../static/icons/linkedin.svg'
+import Fcc from '../static/icons/freecodecamp.svg'
 
 const ContactSection = styled.div`
   position: relative;
   width: 100%;
-  padding: 2rem;
+  padding: 3rem;
   padding-top: 6rem;
   min-height: 100%;
   overflow: auto;
   z-index: 1;
   background-image: linear-gradient(${palette.darker}ff, ${palette.dark}88);
   display: flex;
+  flex-direction: row;
   }
 `
 
-const Image = styled.div`
-  position: absolute;
+const ContactSubSection = styled.div`
   flex: 1;
-  height: 30vw;
-  width: 30vw;
-  top: 25%;
-  left: 35%;
-  border-radius: 50%;
-  background: url(${Yoni}) no-repeat center center;
-  background-size: cover;
-  z-index: 5;
+`
+const ContactInformationContainer = styled(ContactSubSection)`
+  display: flex;
+  flex-direction: column;
+  padding: 16px;
+`
+const ContactInformation = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  max-width: 20rem;
+  color: ${palette.lighter};
+  margin: 8px;
+  box-shadow: 0 0 25px ${palette.dark}44, 0 0 35px ${palette.primary}aa inset;
+  border: 2px solid white;
+  border-image: linear-gradient(to right, ${palette.primary}cc, ${palette.light}ff);
+  border-image-slice: 1;
   div {
+    position: relative;
+    left: -46px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 16px;
+    margin: 16px;
+    width: 60px;
+    height: 60px;
     border-radius: 50%;
-    height: 100%;
-    width: 100%;
-    background: linear-gradient(${palette.dark}ee, ${palette.primary}77);
+    box-shadow: 0 0 25px ${palette.dark}44, 0 0 25px ${palette.primary}44 inset;
+    filter: drop-shadow(0px 0px 8px ${palette.dark});
+    background: linear-gradient(${palette.dark}fe, ${palette.primary}ee);
+    svg {
+      color: ${palette.light};
+    }
   }
-`
-const Ring = styled.div`
-  position: absolute;
-  flex: 1;
-  border-radius: 50%;
-`
-
-const Stripe = styled(Ring)`
-  height: 40vw;
-  width: 45vw;
-  top: 20%;
-  left: 27%;
-  z-index: 6;
-  border-bottom: 5vw solid ${palette.lighter};
-  transform: rotateX(75deg);
-  animation: ${borderGrow} 1.3s ease;
-`
-const StripeB = styled(Ring)`
-  height: 40vw;
-  width: 45vw;
-  top: 20%;
-  left: 27%;
-  z-index: 1;
-  border: 5vw solid ${palette.lighter};
-  border-bottom: none;
-  box-shadow: 0 0 5vw ${palette.lighter}af;
-  transform: rotateX(75deg);
-  animation: ${borderGrow} 1.3s ease;
-`
-const Stripe2 = styled(Ring)`
-  height: 40vw;
-  width: 55vw;
-  top: 20%;
-  left: 22%;
-  z-index: 7;
-  border-bottom: 4vw solid ${palette.light};
-  border-top: none;
-  transform: rotateX(70deg);
-  animation: ${borderGrow} 0.3s ease;
-`
-const Stripe2B = styled(Ring)`
-  height: 40vw;
-  width: 55vw;
-  top: 20%;
-  left: 22%;
-  z-index: 2;
-  border: 4vw solid ${palette.light};
-  border-bottom: none;
-  box-shadow: 0 0 6vw ${palette.light}af;
-  transform: rotateX(70deg);
-  animation: ${borderGrow} 0.3s ease;
-`
-const Stripe3 = styled(Ring)`
-  height: 40vw;
-  width: 70vw;
-  top: 20%;
-  left: 15%;
-  z-index: 8;
-  border-bottom: 3vw solid ${palette.primary};
-  border-top: none;
-  transform: rotateX(65deg);
-  animation: ${borderGrow} 0.3s ease;
-`
-const Stripe3B = styled(Ring)`
-  height: 40vw;
-  width: 70vw;
-  top: 20%;
-  left: 15%;
-  z-index: 3;
-  border: 3vw solid ${palette.primary};
-  border-bottom: none;
-  box-shadow: 0 0 50px ${palette.primary}af;
-  transform: rotateX(65deg);
-  animation: ${borderGrow} 0.3s ease;
-`
-
-const Stripe4 = styled(Ring)`
-  height: 40vw;
-  width: 80vw;
-  top: 20%;
-  left: 10%;
-  z-index: 8;
-  border-bottom: 3vw solid ${palette.dark};
-  border-top: none;
-  transform: rotateX(60deg);
-  animation: ${borderGrow} 0.3s ease forwards;
-`
-const Stripe4B = styled(Ring)`
-  height: 40vw;
-  width: 80vw;
-  top: 20%;
-  left: 10%;
-  z-index: 3;
-  border: 3vw solid ${palette.dark};
-  border-bottom: none;
-  box-shadow: 0 0 50px ${palette.dark}af;
-  transform: rotateX(60deg);
-  animation: ${borderGrow} 0.3s ease forwards;
-`
-
-const Stripe5 = styled(Ring)`
-  height: 40vw;
-  width: 90vw;
-  top: 20%;
-  left: 5%;
-  z-index: 8;
-  border-bottom: 3vw solid ${palette.darker};
-  border-top: none;
-  transform: rotateX(55deg);
-  animation: ${borderGrow} 0.3s ease forwards;
-`
-const Stripe5B = styled(Ring)`
-  height: 40vw;
-  width: 90vw;
-  top: 20%;
-  left: 5%;
-  z-index: 3;
-  border: 3vw solid ${palette.darker};
-  border-bottom: none;
-  box-shadow: 0 0 50px ${palette.darker}af;
-  transform: rotateX(55deg);
-  animation: ${borderGrow} 0.3s ease forwards;
+  p,
+  a {
+    margin: 0;
+    padding: 12px;
+    color: ${palette.light};
+    text-shadow: 0 0 20px ${palette.light};
+    transition: color 0.3s ease;
+  }
+  a:hover {
+    color: ${palette.lighter};
+  }
 `
 
 const About = () => (
   <ContactSection>
-    <Image>
+    <ContactInformationContainer>
+      <ContactInformation>
+        <div>
+          <Email />
+        </div>
+        <p>tomas.sl@hotmail.com</p>
+      </ContactInformation>
+      <ContactInformation>
+        <div>
+          <Phone />
+        </div>
+        <p>+54 11 3110-4987 </p>
+      </ContactInformation>
+      <ContactInformation>
+        <div>
+          <Github />
+        </div>
+        <a href="https://github.com/TomasSuarezL">TomasSuarezL</a>
+      </ContactInformation>
+      <ContactInformation>
+        <div>
+          <LinkedIn />
+        </div>
+        <a href="https://www.linkedin.com/in/tomas-suarez-lissi-62a180147/">Tomás Suarez Lissi</a>
+      </ContactInformation>
+      <ContactInformation>
+        <div>
+          <Fcc />
+        </div>
+        <a href="https://www.freecodecamp.org/tomassuarezl">@tomassuarezl</a>
+      </ContactInformation>
+    </ContactInformationContainer>
+    <ContactSubSection>
       <div />
-    </Image>
-    <Stripe />
-    <StripeB />
-    <Stripe2 />
-    <Stripe2B />
-    <Stripe3 />
-    <Stripe3B />
-    <Stripe4 />
-    <Stripe4B />
-    <Stripe5 />
-    <Stripe5B />
+    </ContactSubSection>
   </ContactSection>
 )
 
